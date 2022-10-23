e5bff2212d1ff784f9db0ef30d02b57c36ad3789
WolfgangPolska <wolfgangpolska@gmail.com>
2022-10-23 14:09:18 +0200

    Initial commit

---
+++ ConfigurableShelfCapacity.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using UnityEngine;
+using Verse;
+using HarmonyLib;
+
+namespace LupineWitch.ConfigurableShelfCapacity
+{
+    [StaticConstructorOnStartup]
+    public static class ConfigurableShelfCapacity
+    {
+        static ConfigurableShelfCapacity()
+        {
+            ConfigurableShelfCapacitySettings.ShelfDef =  DefDatabase<ThingDef>.GetNamed("Shelf");
+            ConfigurableShelfCapacitySettings.ShelfSmallDef =  DefDatabase<ThingDef>.GetNamed("ShelfSmall");
+            ConfigurableShelfCapacitySettings.ApplySettings();
+
+            Harmony.DEBUG = true;
+            var harmony = new Harmony("patch.shelfutils.lupinewitch.mods");
+            harmony.PatchAll();
+        }
+    }
+    
+    public class ConfigurableShelfCapacityMod : Mod
+    {
+        public ConfigurableShelfCapacitySettings Settings;
+
+        public ConfigurableShelfCapacityMod(ModContentPack content) : base(content)
+        {
+            Settings = GetSettings<ConfigurableShelfCapacitySettings>();
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            
+            Listing_Standard listingStandard = new Listing_Standard();
+            listingStandard.Begin(inRect);
+
+            listingStandard.Label(string.Format("Items should tower up to {0} stacks per cell (minimum 2)", ConfigurableShelfCapacitySettings.SplitVisualStackCount));
+            string towerThresholdBuffer = ConfigurableShelfCapacitySettings.SplitVisualStackCount.ToString();
+            listingStandard.IntEntry(ref ConfigurableShelfCapacitySettings.SplitVisualStackCount, ref towerThresholdBuffer);
+
+            listingStandard.Label(string.Format("Minimum capacity is {0}, maximum capacity is {1}", ConfigurableShelfCapacitySettings.MIN_SHELF_CAPACITY, ConfigurableShelfCapacitySettings.MAX_SHELF_CAPACITY));
+            listingStandard.Label("Regular Shelf Max Per Cell Capacity");
+            string shelfBuffer = ConfigurableShelfCapacitySettings.RegularShelfPerCellCapacity.ToString();
+            listingStandard.IntEntry(ref ConfigurableShelfCapacitySettings.RegularShelfPerCellCapacity, ref shelfBuffer);
+            
+            listingStandard.Label("Small Shelf Max Per Cell Capacity");
+            string smallShelfBuffer = ConfigurableShelfCapacitySettings.SmallShelfPerCellCapacity.ToString();
+            listingStandard.IntEntry(ref ConfigurableShelfCapacitySettings.SmallShelfPerCellCapacity, ref smallShelfBuffer);
+            listingStandard.End();
+
+            ConfigurableShelfCapacitySettings.SplitVisualStackCount = ConfigurableShelfCapacitySettings.SplitVisualStackCount.Clamp(2, int.MaxValue);
+            ConfigurableShelfCapacitySettings.SmallShelfPerCellCapacity = ConfigurableShelfCapacitySettings.SmallShelfPerCellCapacity.Clamp(ConfigurableShelfCapacitySettings.MIN_SHELF_CAPACITY, ConfigurableShelfCapacitySettings.MAX_SHELF_CAPACITY);
+            ConfigurableShelfCapacitySettings.RegularShelfPerCellCapacity = ConfigurableShelfCapacitySettings.RegularShelfPerCellCapacity.Clamp(ConfigurableShelfCapacitySettings.MIN_SHELF_CAPACITY, ConfigurableShelfCapacitySettings.MAX_SHELF_CAPACITY);
+
+            base.DoSettingsWindowContents(inRect);
+        }
+
+        public override string SettingsCategory()
+        {
+            return "Configurable Shelf Capacity";
+        }
+
+        public override void WriteSettings()
+        {
+            ConfigurableShelfCapacitySettings.ApplySettings();
+            base.WriteSettings();
+        }
+    }
+}
---
+++ ConfigurableShelfCapacitySettings.cs
@@ -0,0 +1,38 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace LupineWitch.ConfigurableShelfCapacity
+{
+    public class ConfigurableShelfCapacitySettings : ModSettings
+    {
+        public const int MAX_SHELF_CAPACITY = 80;
+        public const int MIN_SHELF_CAPACITY = 1;
+
+        public static int SmallShelfPerCellCapacity = 3;
+        public static int RegularShelfPerCellCapacity = 3;
+        public static int SplitVisualStackCount = 3;
+
+        public static ThingDef ShelfDef;
+        public static ThingDef ShelfSmallDef;
+
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref SmallShelfPerCellCapacity, "SmallShelfPerCellCapacity",3);
+            Scribe_Values.Look(ref RegularShelfPerCellCapacity, "RegularShelfPerCellCapacity",3);
+            Scribe_Values.Look(ref SplitVisualStackCount, "SplitVisualStackCount", 3);
+            base.ExposeData();
+        }
+
+        public static void ApplySettings()
+        {
+            ShelfDef.building.maxItemsInCell = RegularShelfPerCellCapacity;
+            ShelfSmallDef.building.maxItemsInCell = SmallShelfPerCellCapacity;
+        }
+    }
+}
